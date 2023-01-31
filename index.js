c7a181a0f4a3e193b8c526709ed312f7b736026b
Michelle <choi.michelle.12@gmail.com>
2023-01-31 10:42:44 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob";
+const leastFave = "me";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    // console.log(customerName);
+}
+
+// upperCaseCustomerName()
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFave = 'yo';
+}
